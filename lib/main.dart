3260fb79dba7bd8700839c6b51690ff1fb1c7c06
kazuma <kazumazuma1124@gmail.com>
2020-01-26 11:55:43 +0900

    reordablelistのテンプレ導入

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ReorderableListView',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'ReorderableListView Sample'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +26,81 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Model> modelList;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    modelList = [];
+    List<String> titleList = ["Title A", "Title B", "Title C"];
+    List<String> subTitleList = ["SubTitle A", "SubTitle B", "SubTitle C"];
+    for (int i = 0; i < 3; i++) {
+      Model model = Model(
+        title: titleList[i],
+        subTitle: subTitleList[i],
+        key: i.toString(),
+      );
+      modelList.add(model);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+          child: ReorderableListView(
+            padding: EdgeInsets.all(10.0),
+            header: Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey,
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  "This is header",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+            onReorder: (oldIndex, newIndex) {
+              if (oldIndex < newIndex) {
+                // removing the item at oldIndex will shorten the list by 1.
+                newIndex -= 1;
+              }
+              final Model model = modelList.removeAt(oldIndex);
+
+              setState(() {
+                modelList.insert(newIndex, model);
+              });
+            },
+            children: modelList.map(
+              (Model model) {
+                return Card(
+                  elevation: 2.0,
+                  key: Key(model.key),
+                  child: ListTile(
+                    leading: const Icon(Icons.people),
+                    title: Text(model.title),
+                    subtitle: Text(model.subTitle),
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        ));
   }
+}
+
+class Model {
+  final String title;
+  final String subTitle;
+  final String key;
+
+  Model({
+    @required this.title,
+    @required this.subTitle,
+    @required this.key,
+  });
 }
